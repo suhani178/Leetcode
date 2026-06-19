62335fe2a5a4998d63247de84b24e5f20c52aa9c
Suhani Kumari <tannuu17@gmail.com>
2026-06-19 23:01:41 +0530

    Time: 473 ms (19.77%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT DISTINCT AUTHOR_ID AS ID 
-FROM VIEWS
-WHERE AUTHOR_ID = VIEWER_ID ORDER BY 1;
+SELECT DISTINCT author_id as id from Views 
+where author_id = viewer_id
+order by author_id asc;
